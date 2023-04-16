d2d487b37bb566c9581994cb62123c39884efd29
hovhannes-margaryan <hovhannes-margaryan@student.kuleuven.be>
2023-04-16 15:19:58 +0400

    Bug fixes in train

--- inference.py
+++ inference.py
@@ -3,27 +3,30 @@ from torchvision import transforms
 from argparse import ArgumentParser
 from torch.utils.data import DataLoader
 from CustomDataset import CustomDataset
+from models import VisionTransformer
 from modules import GeneralMLightningModule
+import torch.nn.functional as F
 from statistics import mean
 import tqdm
 
 parser = ArgumentParser()
-parser.add_argument("--pretrained_model_path",  help="path to the pretrained model", type=str)
-parser.add_argument("--test_parquet_path",  help="path to the train parquet", type=str)
-parser.add_argument("--device", default="cuda",  help="training and validation batch size", type=str)
-parser.add_argument("--batch_size", default=128,  help="training and validation batch size", type=int)
+parser.add_argument("--pretrained_model_path", help="path to the pretrained model", type=str)
+parser.add_argument("--test_parquet_path", help="path to the train parquet", type=str)
+parser.add_argument("--device", default="cuda", help="training and validation batch size", type=str)
+parser.add_argument("--batch_size", default=128, help="training and validation batch size", type=int)
 parser.add_argument("--image_size", default=224, help="size of the image for training", type=int)
 
 if __name__ == "__main__":
     args = parser.parse_args()
     device = torch.device(args.device)
-    test_transform = transforms.Compose([transforms.Resize((args.image_size, args.image_size)),
-                                               transforms.ToTensor(),
-                                               transforms.Normalize([0.49139968, 0.48215841, 0.44653091],
-                                                                    [0.24703223, 0.24348513, 0.26158784])])
+    test_transform = transforms.Compose([
+        transforms.Resize((args.image_size, args.image_size)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.49139968, 0.48215841, 0.44653091],
+                             [0.24703223, 0.24348513, 0.26158784])])
     test_dataset = CustomDataset(args.test_parquet_path, test_transform)
     test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
-                                       drop_last=False, num_workers=4)
+                                 drop_last=False, num_workers=4)
 
     model = GeneralMLightningModule.load_from_checkpoint(args.pretrained_model_path)
     model.eval()
@@ -33,8 +36,10 @@ if __name__ == "__main__":
         for batch in tqdm.tqdm(test_dataloader):
             imgs, labels = batch
             preds = model(imgs)
+            loss = F.cross_entropy(preds, labels)
             acc = (preds.argmax(dim=-1) == labels).float().mean()
             print(f"acc: {acc}")
+            print(f"loss: {loss}")
             acc_all.append(acc.item())
         print(f"{acc_all}")
         print(f"test acc: {mean(acc_all)}")
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 python3 train.py --num_classes=20 \
- --pretrained_model_path="../models/exp1/lightning_logs/version_0/checkpoints/epoch=127-step=36224.ckpt" \
+ --pretrained_model_path="" \
  --train_parquet_path="../train.parquet" \
  --validation_parquet_path="../validation.parquet" \
  --batch_size=128 \
  --device="cuda" \
  --image_size=224 \
- --max_epochs=500 \
+ --max_epochs=178 \
  --model_checkpoint_path="../models/exp1" \
  --learning_rate=3e-4
